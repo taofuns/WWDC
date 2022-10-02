e5376b5af29b7e3e4d4bc5ef825d4ae86ff9e2a4
taofuns <leisure_tl@163.com>
2022-10-02 21:49:49 +0800

    Initial Commit

---
+++ WWDC/WWDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCApp.swift
+//  WWDC
+//
+//  Created by Leon on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct WWDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
